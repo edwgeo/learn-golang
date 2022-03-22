1fec546b6dd715750e9467c7db73c37f9c1d7074
Edwin George <edwgeo@gmail.com>
2022-03-22 11:26:32 -0700

    hello world with analysis of behind the scenes

---
+++ helloworld/main.go
@@ -0,0 +1,12 @@
+// packages can either be executable or reusable (to be used by other packages).
+// Executable packages must be named "main" and must have a func main() defined
+package main
+
+// importing from the "fmt" package
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
