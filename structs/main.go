8d01e4b03ae2fd17525b9c25c68383b7cafd3bf5
Edwin George <edwgeo@gmail.com>
2022-05-06 23:22:42 -0700

    finished up the structs section

--- structs/main.go
+++ structs/main.go
@@ -24,12 +24,15 @@ func main() {
 		},
 	}
 
+	// shortcut in Go - if our parameter is *person (as it is in receiver), Go will turn a person into a *person
 	jim.updateName("jimmy")
 	jim.print()
 }
 
-func (p person) updateName(newName string) {
-	p.firstname = newName // this doesn't work - why? because we are passing by value, so we only change a copy of the person that is passed in
+// *person is a '*' in front of a type ('person') - the '*' is part of a type description
+// *pointerToPerson is a '*' in front of an actual pointer ('pointerToPerson') - the '*' is an operator
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstname = newName // this doesn't work - why? because we are passing by value, so we only change a copy of the person that is passed in
 	// you can make this work by putting *person in the receiver param, which says you want to receive the param as a pointer
 }
 
